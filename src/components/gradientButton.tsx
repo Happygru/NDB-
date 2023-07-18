a63a5bbe2461e0135ded3c17d52748c7e6fcdf1c
happygru <happygru237@gmail.com>
2023-07-18 07:52:44 -0400

    update:header

---
+++ src/components/gradientButton.tsx
@@ -0,0 +1,7 @@
+const gradientButton = ({ text }: any) => {
+  <button className="color-white border bg-gradient-to-r from-[#F89B7D] to-[#761785] p-4">
+    {text}
+  </button>;
+};
+
+export default gradientButton;
