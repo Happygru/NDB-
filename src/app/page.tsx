4f7633f20ae75043348e3c7e49ac2ac4ecf29ae5
happygru <happygru237@gmail.com>
2023-07-18 09:04:57 -0400

    update: hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+"use client";
 
-import Logo from "@/assets/images/logo.svg";
-import HeroImage from "@/assets/images/hero_background.svg";
+import Image from "next/image";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import Logo from "../../public/images/logo.svg";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init();
+  });
+
   return (
     <div className="w-full">
       {/* header */}
@@ -30,9 +38,28 @@ export default function Home() {
         </div>
       </div>
       {/* hero section */}
-      <div className="w-full bg-black relative">
-        <Image src={HeroImage} className="w-full" alt="HeroImage" />
-        <div className="w-[70%] absolute h-full top-0 left-0 flex items-center justify-center"></div>
+      <div
+        className="w-full relative h-[60vw] max-h-[calc(100vh-86px)] bg-cover flex items-center justify-center"
+        style={{
+          backgroundImage: "url('images/hero_background.svg')",
+        }}
+      >
+        <div
+          className="w-[60%] top-0 left-0 flex flex-col items-start gap-10"
+          data-aos="fade-up"
+        >
+          <p className="text-[46px] break-words text-white w-full leading-tight ">
+            Transforming nuclear waste into
+            <br /> never-recharge
+            <br /> cell phones and watches
+          </p>
+          <a
+            href="#"
+            className="text-white rounded-[50px] bg-white/20 px-16 text-lg py-3 leading-none transition-all duration-200 hover:bg-white/25"
+          >
+            Read more
+          </a>
+        </div>
       </div>
     </div>
   );
