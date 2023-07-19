21089ca944dacd3f08df60e44ba546c8dfad2a16
happy7525 <happydeveloper0816@gmail.com>
2023-07-19 10:42:54 -0400

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "hero-background": "url('@/assets/images/hero_background.svg')",
       },
+      fontFamily: {
+        mainFont: "Suisse BP Int'l",
+      },
+      backgroundColor: {
+        mainColor: "#141414",
+      },
+      color: {
+        mainColor: "#141414",
+      },
+    },
+    screens: {
+      phone: "531px",
+      table: "856px",
+      display: "1241px",
     },
   },
   plugins: [],
